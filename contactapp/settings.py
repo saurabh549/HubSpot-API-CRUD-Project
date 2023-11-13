5a3a515c8cba0e01094bc9ba318db39bd589b208
Saurabh Sharma <83974389+100rabh56@users.noreply.github.com>
2023-11-13 23:02:50 +0530

    Deployements setp fixes

--- contactapp/settings.py
+++ contactapp/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'contacts/static/contacts'
+STATIC_ROOT = os.path.join(BASE_DIR,'contacts/static/contacts')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'contacts/static/contacts'),  # Add this line if you have a 'static' directory in your project root
 ]
