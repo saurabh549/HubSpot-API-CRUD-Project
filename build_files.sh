3c5f47297f9e19ac1671de19dc6884a7ef21e14e
Saurabh Sharma <83974389+100rabh56@users.noreply.github.com>
2023-11-13 22:47:50 +0530

    Deployements setp fixes

--- build_files.sh
+++
@@ -1,5 +0,0 @@
-echo "BUILD START"
-apt-get install -y libsqlite3-dev
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD FINISH"
--- contactapp/settings.py
+++ contactapp/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'contactapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -120,7 +120,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Add this line if you have a 'static' directory in your project root
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
